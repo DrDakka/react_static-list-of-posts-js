935ecbf2618fd8755ad5ee5b348a8eb7eb2ad196
Tim <drdakkadakka@gmail.com>
2025-04-08 15:49:04 +0200

    3rd

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,6 @@ export const data = postsFromServer.map(post => ({
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
-    <PostList data={data} key={data.id} />
+    <PostList data={data} />
   </section>
 );
